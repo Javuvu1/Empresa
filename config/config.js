d42cb14e69565c09e951eb58d19095d45920c213
. <javiercastillolimon@gmial.com>
2025-03-03 10:24:52 +0100

    actualizo public

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Importar libreria para manejo de ficheros de configuración dependiendo de la variable de entorno NODE_ENV
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV || "development"}`
+    path: `.env.${process.env.NODE_ENV || "development"}`
 });
 
   
