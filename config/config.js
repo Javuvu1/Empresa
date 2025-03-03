7aafc5e067e702f58450c7b29f3195ddddb8d29d
. <javiercastillolimon@gmial.com>
2025-03-03 09:44:53 +0100

    cambio config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 // Importar libreria para manejo de ficheros de configuración dependiendo de la variable de entorno NODE_ENV
 require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV || "development"}`,
-  });
+  path: `.env.${process.env.NODE_ENV || "development"}`
+});
+
   
   module.exports = {
     port: process.env.PORT || 3000,
